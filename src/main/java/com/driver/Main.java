2579a52ab11e47053e6b6642074d811377972c13
“[Chandan <[chandan.kumar0510@gmail.com]>
2023-03-29 23:20:03 +0530

    Getter and Setter Name Changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj= new RWOnly();
-        obj.setName("Nikhil");
-        System.out.println(obj.getName());
+        obj.writeOnly ("Nikhil");
+        System.out.println(obj.readOnly ());
     }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,11 +3,11 @@ package com.driver;
 public class RWOnly {
         private String name;
 
-        public String getName() {
+        public String readOnly () {
             return name;
         }
 
-        public void setName(String name) {
+        public void writeOnly (String name) {
             this.name = name;
         }
 
