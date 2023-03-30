fd6c066cde975dfd15c6f92f73a09a3caa1654b7
“[Chandan <[chandan.kumar0510@gmail.com]>
2023-03-30 20:42:35 +0530

    Modified Again

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -11,4 +11,11 @@ public class RWOnly {
             this.name = name;
         }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
